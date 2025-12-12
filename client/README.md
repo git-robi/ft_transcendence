a122691191ced30c0d0ca7f13a2ec5951e29c7d4
Roberta Giambona <giambonaroberta0@gmail.com>
2025-12-12 15:23:31 +0100

    set up client with react and tailwind

--- client/README.md
+++
@@ -1,1 +0,0 @@
-1. Install npm latest version. Run: npm install -g npm@11.7.0 inside client
---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <>
+    <div className="bg-blue-500">app</div>
+    </>
+  )
+}
+
+export default App;
