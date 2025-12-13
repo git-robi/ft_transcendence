30f9459654dfd862dc7b219123e14b955e1f155c
Roberta Giambona <giambonaroberta0@gmail.com>
2025-12-13 13:06:21 +0100

    feat: added swagger for api documentation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,23 @@
 import express, {Request, Response} from "express";
 import dotenv from "dotenv"
 
+// swagger
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerOptions from "./swaggerOptions";
+
+const app = express();
+
+// Generate the OpenAPI specification
+const specs = swaggerJsdoc(swaggerOptions);
+
+// Serve the documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 //import test route
 import test from "./routes/test"
 
 dotenv.config();
-
-const app = express();
-
 
 app.use(express.json()); //allows JSON request bodies
 app.use("/api/v1/test",test);
--- server/src/routes/test.ts
+++ server/src/routes/test.ts
@@ -2,6 +2,25 @@ import express, {Request, Response} from "express";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/test:
+ *   get:
+ *     summary: Health check endpoint
+ *     description: Returns a message indicating that the system is working
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Test endpoint is working
+ */
+
 //healthcheck endpoint
 router.get("/", (req: Request, res: Response) => {
     res.status(200).json({message: "Test endpoint is working"});
