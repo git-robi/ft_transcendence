0f94107630d581fe2a1c9d07f83d2fcb4df86933
Marc Fleury <mfleury03@mailo.com>
2026-01-20 01:13:08 +0100

    updated entrypoint script for prisma to copy created migrations into bind mount

--- docker/requirements/backend/entrypoint.sh
+++ docker/requirements/backend/entrypoint.sh
@@ -3,7 +3,9 @@
 set -e
 npx prisma migrate dev --name="$GIT_BRANCH"
 mkdir -p /migrations
-cp -r /app/prisma/migrations/ /migrations
+cd /migrations
+cp -r /app/prisma/migrations/* .
+cd /app
 npx prisma generate
 npm run dev
 
