de37f49568d55e9be7c19c25d7d160232e80c43e
Marc Fleury <mfleury03@mailo.com>
2026-01-20 15:51:36 +0100

    added a migrate target for Makefile just to build migrations in prisma

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-
 set -e
-npx prisma migrate dev --name="$GIT_BRANCH"
-mkdir -p /migrations
-cd /migrations
-cp -r /app/prisma/migrations/* .
-cd /app
-npx prisma generate
-npm run dev
+if [ "$NODE_ENV" = "production" ]; then
+	npx prisma migrate deploy
+	npx prisma generate
+	npm run build
+	node dist/index.js
+elif [ "$NODE_ENV" = "migration" ]; then
+	npx prisma migrate dev --name="$MIGRATION_NAME"
+	cd /migrations
+	cp -r /app/prisma/migrations/* .
+else
+	npx prisma migrate dev --name="$GIT_BRANCH"
+	mkdir -p /migrations
+	cd /migrations
+	cp -r /app/prisma/migrations/* .
+	cd /app
+	npx prisma generate
+	npm run dev
+fi
 
