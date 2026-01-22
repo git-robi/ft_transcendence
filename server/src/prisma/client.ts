dec453e1b08085e37884b133d498d062528d5790
Marc Fleury <mfleury03@mailo.com>
2026-01-22 16:15:22 +0100

    corected prisma library import - from src/prisma/generated/client

--- server/src/prisma/client.ts
+++ server/src/prisma/client.ts
@@ -1,5 +1,4 @@
-//import { PrismaClient } from './generated';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ 
