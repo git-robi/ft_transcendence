92ba4e6f7858e05076644bf81707a486d90368fd
Roberta Giambona <giambonaroberta0@gmail.com>
2026-02-02 16:12:07 +0100

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Auth from "./APIs/auth";
 import PrivacyPolicy from './routes/routes_tests/PrivacyPolicy';
 import TermsOfService from './routes/routes_tests/TermsOfService';
 import Upload from "./routes/routes_tests/Upload";
+import Register from "./routes/routes_tests/register" 
+import { Navigate  } from 'react-router-dom';
 
 Auth.defaults.withCredentials = true;
 
@@ -43,7 +45,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home user={user} setUser={setUser}/>} />
           <Route path="/login" element={<Login setUser={setUser}/>} />
-
+          <Route path="/register" element={user ? <Navigate to="/" /> : <Register setUser={setUser}/>} />
           <Route path="/upload" element={<Upload />}/>
 
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
