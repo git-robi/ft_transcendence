62edd6e7a1c4cdface604fc68e88ed8e9e668f1d
mpietrza <mpietrza@student.42barcelona.com>
2026-02-03 22:30:42 +0100

    added fetchUser to app.tsx for consistency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SignUp from './routes/SignUp';
 import LogIn from './routes/LogIn';
 import Home from './routes/Home';
@@ -9,9 +9,30 @@ import TermsOfService from './routes/TermsOfService';
 import PrivacyPolicy from './routes/PrivacyPolicy';
 import { LanguageProvider } from './i18n/LanguageProvider';
 import type { PublicUser } from './types';
+import Auth from './APIs/auth';
 
 const App = () => {
   const [user, setUser] = useState<PublicUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await Auth.get("/me");
+        setUser(res.data);
+      } catch (err) {
+        setUser(null);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <LanguageProvider>
