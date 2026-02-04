ddd1f4682a9b8efeffebc750a3f36997219279ec
Roberta Giambona <giambonaroberta0@gmail.com>
2026-02-04 11:32:03 +0100

    Update AUTH_BASE_URL to remove versioning in URL

--- client/src/APIs/auth.ts
+++ client/src/APIs/auth.ts
@@ -1,8 +1,7 @@
 import axios from "axios"
-const AUTH_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api/v1";
+const AUTH_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 //this is creating an axios instance
 export default axios.create({ 
-    baseURL: AUTH_BASE_URL + "/auth",
-    withCredentials: true
+    baseURL: AUTH_BASE_URL + "/auth"
 });
