55977e1e2e38acaecea3ef2b1c0761df0f8a9fa1
Roberta Giambona <giambonaroberta0@gmail.com>
2026-02-08 15:02:20 +0100

    feat: game settings

--- server/prisma/migrations/20260129121552_rgiambon_user_profile/migration.sql
+++ server/prisma/migrations/20260129121552_rgiambon_user_profile/migration.sql
@@ -4,6 +4,9 @@
   - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
 
 */
+ALTER TABLE "apiKeys" DROP CONSTRAINT "apiKeys_userId_fkey";
+
+
 -- DropTable
 DROP TABLE "users";
 
@@ -40,3 +43,5 @@ CREATE UNIQUE INDEX "user_githubId_key" ON "user"("githubId");
 
 -- AddForeignKey
 ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "apiKeys" ADD CONSTRAINT "apiKeys_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
