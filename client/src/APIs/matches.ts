f16e50559628e73cf97cbc6e6feb64c36a24d24d
Roberta Giambona <giambonaroberta0@gmail.com>
2026-03-28 15:06:04 +0100

    fix: vite 404 error

--- client/src/APIs/matches.ts
+++ client/src/APIs/matches.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { attachCsrfInterceptor } from "./csrf";
-const AUTH_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const AUTH_BASE_URL = import.meta.env.VITE_API_URL || "/api/v1";
 
 let api: any;
 
